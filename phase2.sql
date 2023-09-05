61dc4529e12f54455552f1fd6ad28f043c4eca9d
SotirisAle <138309837+SotirisAle@users.noreply.github.com>
2023-09-05 16:02:57 +0300

    phase2.sql
    Create a logical schema

---
+++ phase2.sql
@@ -0,0 +1,64 @@
+CREATE TABLE Staff(
+  staffNo int primary key,
+  staffName varchar(30) NOT NULL,
+  staffSurname varchar(30) NOT NULL
+);
+
+CREATE TABLE Payment(
+  payCode int primary key,
+  payMethod varchar (20) NOT NULL
+  constraint ck_pm check (payMethod='CC' OR payMethod='CH'
+  OR payMethod='CA')
+);
+
+CREATE TABLE Customer(
+  custCode int primary key,
+  custName varchar(30) NOT NULL,
+  custSurname varchar(30) NOT NULL,
+  custPhone varchar(20) NOT NULL
+);
+
+CREATE TABLE Booking(
+  bookCode int primary key,
+  bookDt date NOT NULL,
+  payCode int NOT NULL,
+  custCode int NOT NULL,
+  staffNo int NOT NULL,
+  Foreign Key (payCode) references payment(payCode),
+  Foreign Key (custCode) references Customer(custCode),
+  Foreign Key (staffNo) references Staff(staffNo)
+);
+
+CREATE TABLE Camping(
+  campCode char(3) primary key,
+  campName varchar(50) NOT NULL,
+  numOfEmp int NOT NULL
+);
+
+CREATE TABLE Category(
+  catCode char(1) primary key,
+  areaM2 int NOT NULL,
+  unitCost numeric(4,2)not NULL
+  5
+);
+
+CREATE TABLE Emplacement(
+  campCode char(3) NOT NULL,
+  empNo int NOT NULL,
+  catCode char(1) NOT NULL,
+  Primary Key (campCode,empNo),
+  Foreign Key (campCode) references Camping (campCode),
+  Foreign Key (catCode) references Category (catCode)
+);
+
+CREATE TABLE Rental(
+  bookCode int NOT NULL,
+  campCode char(3) NOT NULL,
+  empNo int NOT NULL,
+  startDt date NOT NULL,
+  endDt date NOT NULL,
+  noPers int NOT NULL,
+  Primary Key (bookCode,campCode,empNo,startDt),
+  Foreign Key (bookCode) references Booking (bookCode),
+  Foreign Key (campCode,empNo) references Emplacement (campCode,empNo)
+);
